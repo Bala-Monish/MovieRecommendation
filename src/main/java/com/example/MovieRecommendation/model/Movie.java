644c9e4cf1e8640b2024de37fc5ebc8ffceb8d4d
Arun Krishnamurthy <arun.km.279@gmail.com>
2020-02-06 14:41:47 -0600

    adding model for movie and genre

---
+++ src/main/java/com/example/MovieRecommendation/model/Movie.java
@@ -0,0 +1,40 @@
+package com.example.MovieRecommendation.model;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String description;
+    private String imageURL;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
