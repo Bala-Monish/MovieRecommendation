a41aa1638172b773f6ea5d2c25c8a38ca117cf96
Arun Krishnamurthy <arun.km.279@gmail.com>
2020-02-09 23:22:33 -0600

    setting up basic eureka service to work with movie service

---
+++ MovieRecommendationDiscoveryService/src/main/java/com/example/MovieRecommendation/discovery/MovieRecommendationDiscoveryService/MovieRecommendationDiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.MovieRecommendation.discovery.MovieRecommendationDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MovieRecommendationDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommendationDiscoveryServiceApplication.class, args);
+	}
+}
