4c657be88303231e5daef86065d7c4717d22edcf
Arun Krishnamurthy <arun.km.279@gmail.com>
2020-02-09 23:43:49 -0600

    properties and annotation changes to enable eureka to pick up movie service

--- MoviesAPIService/src/main/java/com/example/MovieRecommendation/api/movies/MoviesAPIService/MoviesApiServiceApplication.java
+++ MoviesAPIService/src/main/java/com/example/MovieRecommendation/api/movies/MoviesAPIService/MoviesApiServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.MovieRecommendation.api.movies.MoviesAPIService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MoviesApiServiceApplication {
 
 	public static void main(String[] args) {
