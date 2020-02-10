d96eff63968398dc03907483dc0af2172436dc17
Arun Krishnamurthy <arun.km.279@gmail.com>
2020-02-09 23:34:11 -0600

    adding a basic movies service project
    no functionality yet

---
+++ MoviesAPIService/src/main/java/com/example/MovieRecommendation/api/movies/MoviesAPIService/MoviesApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieRecommendation.api.movies.MoviesAPIService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApiServiceApplication.class, args);
+	}
+
+}
---
+++ MoviesAPIService/src/test/java/com/example/MovieRecommendation/api/movies/MoviesAPIService/MoviesApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieRecommendation.api.movies.MoviesAPIService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
