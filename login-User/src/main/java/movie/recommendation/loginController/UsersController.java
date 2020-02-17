0c8c9954c4bb63185b71555f73b0f0c19b578def
Sagar Limaye <sagarl3232@hotmail.com>
2020-02-17 15:37:30 -0600

    removed unused imports login service

--- login-User/src/main/java/movie/recommendation/loginController/UsersController.java
+++ login-User/src/main/java/movie/recommendation/loginController/UsersController.java
@@ -1,7 +1,6 @@
 package movie.recommendation.loginController;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -10,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
