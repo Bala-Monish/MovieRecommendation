3787cf524add636ce2142cb56d2437f3efee6fcb
Sagar Limaye <sagarl3232@hotmail.com>
2020-02-17 15:46:54 -0600

    Get movies from movies service in recommender service

--- recommender/src/main/java/com/movierecommendation/recommender/controller/RecommenderController.java
+++ recommender/src/main/java/com/movierecommendation/recommender/controller/RecommenderController.java
@@ -5,20 +5,33 @@ import java.util.stream.Stream;
 
 import com.movierecommendation.recommender.model.Genre;
 import com.movierecommendation.recommender.model.Movie;
-import com.movierecommendation.recommender.model.Preference;
+import com.netflix.discovery.EurekaClient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class RecommenderController {
-    
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    EurekaClient discoveryClient;
+
     @GetMapping("/recommendations")
-    public Movie[] getRecommendations(Preference pref) {
-        Stream<Genre> genres = Arrays.stream(pref.getUserPreferences());
-        Movie[] movies = pref.getMovies();
+    public Movie[] getRecommendations(Genre[] prefs) {
+        Stream<Genre> genres = Arrays.stream(prefs);
+
+        String url = discoveryClient.getNextServerFromEureka("movies-ws", false).getHomePageUrl();
         
-        return Arrays.stream(movies).filter(m -> genres.anyMatch(g -> g.getId() == m.getGenre().getId())).toArray(Movie[]::new);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url).pathSegment("movies");
+
+        Movie[] movies = restTemplate.getForObject(builder.build().toUriString(), Movie[].class);
+
+        return Arrays.stream(movies).filter(m -> genres.anyMatch(g -> g.getId() == m.getGenre().getId())).limit(3).toArray(Movie[]::new);
     }
 
 }
