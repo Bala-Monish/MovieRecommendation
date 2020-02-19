9f3976b07368e57eb7c162249ee4608f577a72b2
deshicabolledula <42794416+deshicabolledula@users.noreply.github.com>
2020-02-19 17:07:46 -0600

    ChangestoRegistration

--- Registration/src/main/java/com/example/Registration/Controller/RegistrationController.java
+++ Registration/src/main/java/com/example/Registration/Controller/RegistrationController.java
@@ -1,26 +1,26 @@
 package com.example.Registration.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Registration.Model.UserRegistrationModel;
 import com.example.Registration.dao.RegistrationRepository;
 
-@Controller
+@RestController
+@RequestMapping(path="/mrs")
 public class RegistrationController {
 
 	@Autowired
 	private RegistrationRepository regrep;
 	
-	@PostMapping 
-	@ResponseStatus(value = HttpStatus.CREATED)
-	public int save (@RequestBody UserRegistrationModel user) {
-		regrep.save(user);
-		return user.getId();
+	 @PostMapping("/user") 
+	 		public int save (@RequestBody UserRegistrationModel user) {
+			regrep.save(user);
+			return user.getId();
 	 
 //	UserRegistrationModel n = new UserRegistrationModel();
 //			n.setName(firstName);
@@ -30,4 +30,8 @@ public class RegistrationController {
 //			regrep.save(n);
 //			return "Saved";
 	 }
+		@GetMapping("/status/check")
+		public String status() {
+			return "working";
+		}
 }
--- Registration/src/main/java/com/example/Registration/dao/RegistrationRepository.java
+++ Registration/src/main/java/com/example/Registration/dao/RegistrationRepository.java
@@ -1,7 +1,7 @@
 package com.example.Registration.dao;
 
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Registration.Model.UserRegistrationModel;
@@ -10,7 +10,7 @@ import com.example.Registration.Model.UserRegistrationModel;
 // CRUD refers Create, Read, Update, Delete
 
 @Repository
-public interface RegistrationRepository extends JpaRepository<UserRegistrationModel, String> {
-	 UserRegistrationModel findByid(String id);
+public interface RegistrationRepository extends CrudRepository<UserRegistrationModel, Integer> {
+	 UserRegistrationModel findByid(int id);
 
 }
