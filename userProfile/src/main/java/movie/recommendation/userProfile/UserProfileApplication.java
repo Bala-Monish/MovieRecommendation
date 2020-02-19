dc0e21e8946e0c0ae735860c4ddaab767270d700
Sagar Limaye <sagarl3232@hotmail.com>
2020-02-19 10:33:18 -0600

    Removed user controller and fixed spring boot annotation

--- userProfile/src/main/java/movie/recommendation/userProfile/UserProfileApplication.java
+++ userProfile/src/main/java/movie/recommendation/userProfile/UserProfileApplication.java
@@ -2,13 +2,11 @@ package movie.recommendation.userProfile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
-//@SpringBootApplication
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class UserProfileApplication {
 
 	
