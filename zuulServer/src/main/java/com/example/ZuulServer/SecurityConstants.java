5ebc3ccba848f559eaafcb9cf66570f0a201e2d0
Sagar Limaye <sagarl3232@hotmail.com>
2020-02-21 14:20:53 -0600

    Fixed zuul register url

--- zuulServer/src/main/java/com/example/ZuulServer/SecurityConstants.java
+++ zuulServer/src/main/java/com/example/ZuulServer/SecurityConstants.java
@@ -5,5 +5,5 @@ public class SecurityConstants {
     public static final int EXPIRATION_TIME = 300000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/mrs/register";
+    public static final String SIGN_UP_URL = "/mrs/registration/user";
 }
